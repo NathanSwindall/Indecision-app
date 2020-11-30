855c7cecdedbc22c7668d0d97b3d2daf91b7c000
Nathan Swindall <Nathan.swindall20103@gmail.com>
2020-11-30 10:21:24 -0600

    Added some Css to the app

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -89,17 +89,19 @@ class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header  subtitle={subtitle} />
-                <Action hasOptions = {this.state.options.length > 0}
-                        handlePick = {this.handlePick}
-                />
-                <Options options={this.state.options}
-                         handleDeleteOptions = {this.handleDeleteOptions}
-                         handleDeleteOption = {this.handleDeleteOption}
-                         />
-                <AddOptions 
-                        handleAddOptions = {this.handleAddOptions}
-                        
-                />
+                <div className="container">
+                    <Action hasOptions = {this.state.options.length > 0}
+                            handlePick = {this.handlePick}
+                    />
+                    <Options options={this.state.options}
+                            handleDeleteOptions = {this.handleDeleteOptions}
+                            handleDeleteOption = {this.handleDeleteOption}
+                            />
+                    <AddOptions 
+                            handleAddOptions = {this.handleAddOptions}
+                            
+                    />
+                </div>
                 <OptionModal selectedOption={this.state.selectedOption}
                              handleClose={this.handleClose}/>
             </div>
